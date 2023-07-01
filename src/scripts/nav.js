e81c75033c72649ab230b578a79328c31230506a
thunderboiii <emil@maple-ink.com>
2023-07-01 19:21:15 +0200

    Lang Button

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -4,3 +4,10 @@ const navbarLinks = document.getElementsByClassName('menu')[0];
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
+
+const dropdownButton = document.getElementsByClassName('dropdown-button')[0];
+const dropdownMenu = document.getElementsByClassName('dropdown-menu')[0];
+
+dropdownButton.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('active');
+});
